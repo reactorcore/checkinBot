50fd8094cff288f453a6a9dcc0b7ab7db41f4fdd
Jarett Engdahl <jaretten@gmail.com>
2017-10-23 18:15:28 -0400

    dumb mistake

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/",function(req,res){
   res.send("success")
 })
 app.post("/checkin",function(req,res){
-  console.log(req.body)
+  console.log(req.text)
   res.send("K")
 })
 app.listen(process.env.PORT)
