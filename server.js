11f99b710db273a4767b6d27e4170d6087732e9a
Jarett Engdahl <jaretten@gmail.com>
2017-10-23 18:29:09 -0400

    dumb mistake

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 let express = require("express")
 let app = express()
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/",function(req,res){
   console.log("Request~",req)
   res.send("success")
 })
 app.post("/checkin",function(req,res){
-  console.log(req.body)
+  console.log(req.body.text)
   console.log(req.text)
   console.log(req.data)
   console.log(req.token)
