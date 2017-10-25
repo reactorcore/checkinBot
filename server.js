55350cabc2383b41a819ace693223f159278704b
Jarett Engdahl <jaretten@gmail.com>
2017-10-25 13:52:55 -0400

    changing to real database

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let express = require("express")
 let app = express()
+var mysql = require('mysql');
 
-var JsonDB = require('node-json-db');
-var db = new JsonDB("myDataBase", true, false);
-
-//getMonth getDate
+var db = mysql.createConnection({
+  host     : process.env.HOST,
+  user     : process.env.USER,
+  password : process.env.PASSWORD
+});
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -12,41 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",function(req,res){
   console.log("Request~",req)
-  res.send("success")
+  res.send("Server-UP")
 })
 app.post("/checkin",function(req,res){
-
-  if(req.body.text === "get"){
-    res.send(db.getData("/"))
-  }
-
-
-  var date = new Date()
-  var month = date.getMonth()
-  var day = date.getDate()
-  var there = true;
-
-console.log(db.getData("/"))
-
-  if(!db.getData("/").month){
-    there = false
-  } else if (!db.getData("/").month.day) {
-    there = false
-  } else if (!db.getData("/").month.day[req.body.user_name]) {
-    there = false
-  }
-
-
-console.log(there)
-
-
-  if(!there){
-    db.push(`/${date.getMonth()}/${date.getDate()}`,req.body.user_name)
-    res.send("Thanks for checking in!")
-  }
-  if(there) {
-    res.send("You're already checked in for today!")
-  }
-
+  console.log(req.body)
 })
 app.listen(process.env.PORT)
