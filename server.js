b47a9dd3171bf1757dd76f1e0034e773158c9a45
Jarett Engdahl <jaretten@gmail.com>
2017-10-25 14:07:56 -0400

    fooling with vars

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ var db = mysql.createConnection({
   user     : process.env.USER,
   password : process.env.PASSWORD
 });
-
+setTimeout(
 db.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
 
-  console.log('connected as id ' + connection.threadId);
+  console.log('connected as id ' + db.threadId);
 });
+, 3000)
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
